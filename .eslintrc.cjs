705a368e2c0273c0e48ce30b321f6d9ddf56b99e
Nurayat Beltayev <F1zm0@localhost.localdomain>
2024-03-31 19:10:08 +0500

    init: created base structure for project

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/function-component-definition': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  }
+};
---
+++ src/views/Garage.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const Garage: FC = () => {
+  return <div />;
+};
+
+export default Garage;
