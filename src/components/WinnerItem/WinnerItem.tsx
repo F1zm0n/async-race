c9e8b1a0917663ea7306cddfb3f8cb74355f800c
Nurayat Beltayev <F1zm0@localhost.localdomain>
2024-04-09 10:12:23 +0500

    init: refactor CarList

---
+++ src/components/WinnerItem/WinnerItem.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import CarApi from '../../api/CarApi';
+import classes from './WinnersItem.module.css';
+import SedanCar from '../UI/SedanCar/SedanCar';
+import { IWinner } from '../../models/api/Winners';
+
+interface WinnersItemProps {
+  winner: IWinner;
+}
+
+const WinnersItem: FC<WinnersItemProps> = ({ winner }) => {
+  const { data } = CarApi.useGetOneCarQuery(winner.id);
+  return (
+    <div
+      style={{ border: `1px solid ${data?.color}` }}
+      className={classes.wrapper}>
+      <div className={classes.item}>{data?.id}</div>
+      <SedanCar color={data?.color as string} />
+      <div className={classes.item}>{data?.name}</div>
+      <div className={classes.item}>{winner.wins}</div>
+      <div className={classes.item}>{winner.time.toFixed(2)}</div>
+    </div>
+  );
+};
+
+export default WinnersItem;
